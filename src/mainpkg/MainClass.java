8c37e7314d1cbceee968fa1a0efe7729f9042702
Damian <dematteodamian@gmail.com>
2025-05-30 22:48:01 -0400

    added error prevention and printing of statistics after coinflips

--- src/mainpkg/MainClass.java
+++ src/mainpkg/MainClass.java
@@ -3,15 +3,23 @@ import java.util.Scanner;
 import java.util.Random;
 
 public class MainClass {
+	static int numCoins, numHeads, numTails;
+	
 	public static void main(String[] args) {
 		Scanner scnr = new Scanner(System.in);
+		numHeads = 0;
+		numTails = 0;
 		
 		System.out.println("Enter the number of coins to be flipped!");
 		
-		int numCoins = scnr.nextInt();
+		numCoins = Math.abs(scnr.nextInt()); // Ensure a non-negative number of coins
+		
 		for (var i = 0; i < numCoins; i++) {	
 			System.out.println(flipCoin());
 		}
+		
+		System.out.println("Percentage of coinflips that landed on heads: " + (((float)numHeads / numCoins) * 100) + "%");
+		System.out.println("Percentage of coinflips that landed on tails: " + (((float)numTails / numCoins) * 100) + "%");
 	}
 	
 	static String flipCoin() {
@@ -19,8 +27,10 @@ public class MainClass {
 		boolean isHeads = rand.nextBoolean();
 		
 	    if (isHeads) {
+	    	numHeads++;
             return "Heads";
         } else {
+            numTails++;
             return "Tails";
         }
 	}
